0a39d5b9035fc63fc48f94985af7448a8d6e9a7b
Shelainy <mayracandea@gmail.com>
2023-10-14 18:52:22 -0300

    all models created

--- library/models.py
+++ library/models.py
@@ -1,50 +1,48 @@
 from django.db import models
 from django.utils import timezone
 
-#Book
+# BOOK
 # id (primary key - automático)
 # name (string), genero (string), escritor (string), editora (string), data_pub (date), description (text)
-#picture (imagem), disponivel (boolean)
+# picture (imagem), disponivel (boolean)
 
-#disponivel: False se o livro for alugado, True quando o livro é devolvido
-#criar um trigger para mudar o valor quando o livro for alugado/devolvido
-#livro não pode ser alugado se o valor = False
+# disponivel: False se o livro for alugado, True quando o livro é devolvido
+# criar um trigger para mudar o valor quando o livro for alugado/devolvido
+# livro não pode ser alugado se o valor = False
 
-#Cliente
+# CLIENTE
 # id (primary key - automático)
 # nome (string), sobrenome (string), cpf (string, unique key), nascimento (date), email (email), 
 # rua (string), numero (string), complemento (string), bairro (string), cep (string)
 
-# Telefone
-# id (primary key - automático)
-# id_cliente (foreing key), telefone (string)
-
-#Cliente-livro
+# CLIENTE-LIVRO
 # id (primary key - automático)
 # id_livro (foreign key), id_cliente (foreign_key), data_alugel (date), data_dev (date), multa (real)
 
-#data_dev: Preenchido de acordo com o valor data_aluguel
-#multa: calculado automaticamente de acordo com a diferença entre o dia atual e data_dev (default = 0)
+# data_dev: Preenchido de acordo com o valor data_aluguel
+# multa: calculado automaticamente de acordo com a diferença entre o dia atual e data_dev (default = 0)
 # (Talvez criar um botão "Ver multa" para chamar uma função que faz o cálculo)
 
-class Book(models.Model):
+class Livro(models.Model):
     nome = models.CharField(max_length=254)
     genero = models.CharField(max_length=50)
     escritor = models.CharField(max_length=100)
     editora = models.CharField(max_length=100)
-    edicao = models.CharField(max_length=25)
+    edicao = models.CharField(max_length=25, blank=True)
     data_pub = models.DateField()
     picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
     disponivel = models.BooleanField(default=True)
 
     def __str__(self) -> str:
-        return f'{self.nome} - {self.escritor}'
+        return self.nome or ''
     
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     sobrenome = models.CharField(max_length=100)
     cpf = models.CharField(max_length=15, unique=True)
     nascimento = models.DateField()
+    telefone_1 = models.CharField(max_length=15)
+    telefone_2 = models.CharField(max_length=15, blank=True) 
     email = models.EmailField(max_length=254, blank=True)
     rua = models.CharField(max_length=100)
     numero = models.CharField(max_length=10)
@@ -53,4 +51,14 @@ class Cliente(models.Model):
     cep = models.CharField(max_length=10)
 
     def __str__(self) -> str:
-        return f'{self.nome} '
+        return self.nome or ''
+
+class Cliente_Livro(models.Model):
+    id_livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data_aluguel = models.DateField(auto_now_add=True)
+    data_dev = models.DateField()
+    multa = models.FloatField(default=0)
+
+#    def __str__(self) -> str:
+#        return self.id_cliente or ''
